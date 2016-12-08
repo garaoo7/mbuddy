c0788e5a29b3a314c03278d62e670483cbaf88bd
Akshit Agarwal <akshitagarwal10@gmail.com>
2016-12-08 14:57:05 +0530

    8-dec 2:56

--- application/modules/Common/views/homepage.php
+++ application/modules/Common/views/homepage.php
@@ -39,5 +39,5 @@
 
 </body>
 <script src="<?php echo base_url("jquery/jquery-3.1.1.min.js");?>"></script>
-<script src="<?php echo base_url("js/user/userJavascript.js");?>"></script>
+<script src="<?php echo base_url("js/javascript.js");?>"></script>
 </html>
